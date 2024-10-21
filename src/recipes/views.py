1e146d15473a09e21032d886cff9a949de81ac20
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-10-21 07:58:40 +0200

    Protect CBV, FBV views

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 
+# to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# to protect function-based views
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+# keep protected
+@login_required
 def home(request):
     return render(request, "recipes/recipes_home.html")
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = "recipes/recipes_overview.html"
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipes/recipe_detail.html"
 
