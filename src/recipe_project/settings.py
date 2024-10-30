59cc9ba5ae090e84b77f63d048c71ae98b6c0f1b
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-10-30 09:43:30 +0100

    Update settings

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["shrouded-island-48591-daf204f3e183.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "shrouded-island-48591-daf204f3e183.herokuapp.com"]
 
 
 # Application definition
