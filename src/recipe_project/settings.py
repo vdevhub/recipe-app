3f85acc3e4daf4424a3dcad8e48fba3ed34f7037
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-10-30 09:13:59 +0100

    Add allowed host

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["shrouded-island-48591-daf204f3e183.herokuapp.com"]
 
 
 # Application definition
